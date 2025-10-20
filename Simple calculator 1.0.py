37df94f7dc58d3bc366e756b189a6e7a1530d19c
LeoAng <86818332+willianang@users.noreply.github.com>
2025-10-20 21:30:53 +0800

    Add files via upload

---
+++ Simple calculator 1.0.py
@@ -0,0 +1,22 @@
+print("Simple calculator")
+number = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+operation = ["1.Add","2.Minus","3.Multiple","4.Devision"]
+for x in operation:
+    print(x)
+selection=int(input("Plese select operation: "))
+if(selection == 1):
+    add = number + number2
+    print(add)
+elif(selection == 2):
+    minus = number - number2
+    print(minus)
+elif(selection == 3):
+    multiple = number * number2
+    print(multiple)
+elif(selection == 4):
+    division = number / number2
+    print(float(division))
+else:
+    print("Error, Plese run again!")
+    
---
+++ Simple calculator 1.1.py
@@ -0,0 +1,29 @@
+print("Simple calculator")
+i = 1
+operation = ["1.Add","2.Minus","3.Multiple","4.Devision","5.End"]
+# Main loop
+while i == 1:
+    number = int(input("Enter a number: "))
+    number2 = int(input("Enter another number: "))
+    print(operation)
+    selection=int(input("Plese select operation: "))
+    if(selection == 1):
+       add = number + number2
+       print(add)
+    elif(selection == 2):
+       minus = number - number2
+       print(minus)
+    elif(selection == 3):
+       multiple = number * number2
+       print(multiple)
+    elif(selection == 4):
+       division = number / number2
+       print(float(division))
+    elif(selection == 5):
+       break
+    else:
+       print("Error, Plese enter a correct option!")
+
+
+
+    
